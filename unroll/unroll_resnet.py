e9f1a46b2622f9fd088e0e06b4139887efc951af
Ronnypetson <rsronnypetson4@gmail.com>
2018-12-15 19:44:24 -0200

    banana

--- unroll/unroll_resnet.py
+++ unroll/unroll_resnet.py
@@ -10,24 +10,30 @@ from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 
-batch_size = 12
+batch_size = 16
 learning_rate = 0.001
 sequence_len = 3 # Odd number
 img_h = 224 #121 #65
 img_w = 224 #161 #49
-rs_dir = '/home/ronnypetson/Downloads/CVPR_Dataset/house_rot1_B0/rolling_shutter/'
-gs_dir = '/home/ronnypetson/Downloads/CVPR_Dataset/house_rot1_B0/gt_end/'
+rs_dir = '/home/ronnypetson/Documents/penncosyvio/data/tango_bottom/af/frames/' #'~/Downloads/CVPR_Dataset/house_rot1_B0/rolling_shutter/'
+gs_dir = '/home/ronnypetson/Documents/penncosyvio/data/tango_top/af/frames/' #'~/Downloads/CVPR_Dataset/house_rot1_B0/gt_end/'
 model_fn = 'rs2gs_resnet.h5'
+
+def fn2int(fn):
+    return int(fn[:-4])
 
 def load_data(rs=rs_dir, gs=gs_dir):
 	rs_files = os.listdir(rs)
+	rs_files.sort(key=fn2int)
 	gs_files = os.listdir(gs)
+	gs_files.sort(key=fn2int)
 
 	model = ResNet50(weights='imagenet')
 	model = Model(inputs=model.input, outputs=model.get_layer('activation_49').output)	# None,7,7,2048, avg_pool, activation_49
 
 	rs_imgs = []
 	gs_imgs = []
+	i = 0
 	for rsf, gsf in zip(rs_files, gs_files):
 		rs_img = cv2.imread(rs_dir+rsf,0)
 		rs_img = cv2.resize(rs_img,(img_h,img_w))
@@ -38,6 +44,8 @@ def load_data(rs=rs_dir, gs=gs_dir):
 		gs_img = cv2.resize(gs_img,(440,440))	# 440,440
 		gs_img = preprocess_input(gs_img)/255.0
 		gs_imgs.append(np.reshape(gs_img,(440,440,1)))
+		if i == 300: break
+		i += 1
 
 	num_imgs = len(rs_imgs)
 	rs_data = []
@@ -75,20 +83,27 @@ else:
 x_train, y_train = load_data()
 
 autoencoder.fit(x_train, y_train,\
-                epochs=2000,\
+                epochs=100,\
                 batch_size=batch_size,\
                 shuffle=True,\
                 validation_data=(x_train, y_train),\
                 callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
 
-decoded_imgs = autoencoder.predict(x_train)
+decoded_imgs = autoencoder.predict(x_train[:12])
 
 n = 12
 plt.figure(figsize=(20, 4))
 for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(y_train[i].reshape(440, 440))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     # display reconstruction
-    ax = plt.subplot(1, n, i + 1)
-    plt.imshow(decoded_imgs[i].reshape(440,440))
+    ax = plt.subplot(2, n, i + n + 1)
+    plt.imshow(decoded_imgs[i].reshape(440, 440))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
